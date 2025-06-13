83428daaa19c10d32a7369e5ccb4f912153e5f1f
OneSword-LCLW <3457800744@qq.com>
2025-06-13 18:54:55 +0800

    Site updated: 2025-06-13 18:54:55

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/06/08/H&NCTF2025/"];function toRandomPost(){
+var posts=["2025/06/08/H&NCTF2025/","2025/06/13/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
